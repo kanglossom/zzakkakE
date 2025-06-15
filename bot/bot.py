fc0374fc99c074b4233cceb859814754e53e4b0f
kanglossom <404notfoundnow@gmail.com>
2025-06-15 19:37:49 +0900

    이름 영문으로 바꿔서 디버깅

--- bot/bot.py
+++ bot/bot.py
@@ -23,47 +23,55 @@ async def on_ready():
     print(f'로그인완료: {bot.user}')
     try:
         bot.tree.clear_commands(guild=None)  # 기존 슬래시 명령어 전부 삭제
-        await bot.tree.sync(guild=None)          # 👈 전역으로 다시 등록
-        print(f"✅ 새 명령어 등록됨:")
+        synced = await bot.tree.sync()  # 👈 전역으로 다시 등록
+        print(f"✅ 명령어{len(synced)}개 등록됨:")
     except Exception as e:
         print(f"❌ 명령어 등록 실패: {e}")
 
 
 # /일정입력 [제목] [날짜]
-@bot.tree.command(name="일정입력", description="일정을 등록합니다")
+@bot.tree.command(name="add_schedule", description="일정을 등록합니다")
 # 이건 제목 = / 눌렀을 때 뜨는 명령어 이름, 설명 = 말그대로 옆에 무슨기능인지 나오는 설명임
 @app_commands.describe(제목="일정 제목", 날짜="날짜 (YYYY-MM-DD)")
 # 이건 입력할때 입력창에 보면 각각 필드를 눌렀을 때 어떤 형식으로 입력하는지 알려주는거임
 # 이 밑에있는 함수가 실제 입력값 받아서 넘어가는 부분인거야
 async def 일정입력(interaction: discord.Interaction, 제목: str, 날짜: str):
-    data = {
-        "title" : 제목,
-        "date" : 날짜,
-        "discord_user": str(interaction.user)
-    }
-    # data로 묶여서 서버에 넘어갈 준비 완료
+    try:
+        data = {
+            "title" : 제목,
+            "date" : 날짜,
+            "discord_user": str(interaction.user)
+        }
+        # data로 묶여서 서버에 넘어갈 준비 완료
 
-    res = requests.post("http://127.0.0.1:8000/schedule/create/", json=data)
-    # Django 서버로 HTTP POST 요청 보냄!
-    if res.status_code == 200:
-        await interaction.response.send_message("일정등록 완료")
-        # 등록이 완료되었을 때 봇이 보내는 메세지가 바로 여기
-    else :
-        await interaction.response.send_message("등록 실패")
-        # 등록에 실패했을 때 봇이 보내는 메세지가 여기!
+        res = requests.post("http://127.0.0.1:8000/schedule/create/", json=data)
+        # Django 서버로 HTTP POST 요청 보냄!
+        if res.status_code == 200:
+            await interaction.response.send_message("일정등록 완료")
+            # 등록이 완료되었을 때 봇이 보내는 메세지가 바로 여기
+        else :
+            await interaction.response.send_message("등록 실패")
+            # 등록에 실패했을 때 봇이 보내는 메세지가 여기!
+    except Exception as e:
+        await interaction.response.send_message(f"에러발생 {e}")
 
 # /일정확인
-@bot.tree.command(name="일정확인", description="등록된 일정을 전부 확인합니다")
+@bot.tree.command(name="view_schedule", description="등록된 일정을 전부 확인합니다")
 async def 일정확인(interaction: discord.Interaction):
-    response = requests.get("http://127.0.0.1:8000/schedule/")
-    if response.status_code == 200:
-        data = response.json()
+    try: 
+        res = requests.get("http://127.0.0.1:8000/schedule/")
+        if res.status_code != 200:
+            await interaction.response.send_message("서버가 응답을 안해요")
+            return
+        
+        data = res.json()
         if not data:
             await interaction.response.send_message("등록된 일정이 없다능")
-        else:
-            msg = "\n".join([f"{item['date']}-{item['title']}"for item in data])
-            await interaction.response.send_message(f"등록된 일정:\n{msg}")
-    else:
+            return
+        msg = "\n".join([f"{item['date']}-{item['title']}"for item in data])
+        await interaction.response.send_message(f"등록된 일정:\n{msg}")
+    
+    except Exception as e:
         await interaction.response.send_message("서버에러")
 
 bot.run(TOKEN) # 봇 실행~
